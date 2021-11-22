aeb395c9e2a511a91c69b40ac63fe0a2f583aa68
happyjane210 <happyjane210@gmail.com>
2021-11-22 12:41:01 +0900

    백엔드 배포

--- subscribe/deploy.bat
+++ subscribe/deploy.bat
@@ -1,4 +1,4 @@
-ssh -i "C:\Users\juh22\OneDrive\문서\Github\weekly-coffee\workspace.pem" ubuntu@ec2-15-165-17-21.ap-northeast-2.compute.amazonaws.com
+ssh -i "C:\Users\juh22\OneDrive\문서\Github\weekly-coffee\myworkspace.pem" ubuntu@ec2-15-165-17-21.ap-northeast-2.compute.amazonaws.com
 
-ssh -i "C:\Users\juh22\OneDrive\문서\Github\weekly-coffee\workspace.pem" ubuntu@ec2-15-165-17-21.ap-northeast-2.compute.amazonaws.com
-ssh -i "C:\Users\juh22\OneDrive\문서\Github\weekly-coffee\workspace.pem" 
+ssh -i "C:\Users\juh22\OneDrive\문서\Github\weekly-coffee\myworkspace.pem" ubuntu@ec2-15-165-17-21.ap-northeast-2.compute.amazonaws.com "cd /home/ubuntu/weekly-coffee/subscribe; git pull; npm install; npm run build; nohup npm start 1>/dev/null 2>&1 &"
+ssh -i "C:\Users\juh22\OneDrive\문서\Github\weekly-coffee\myworkspace.pem" 
