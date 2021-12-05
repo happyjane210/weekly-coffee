63d0188243fcdb1e33a8679da8ee8483698d62b2
happyjane210 <happyjane210@gmail.com>
2021-12-06 04:07:01 +0900

    2021-12-06 포트폴리오 업로드

---
+++ subscribe-server/subscribe-server/src/main/java/com/git/subscribeserver/configuration/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.git.subscribeserver.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+//Swagger 2 UI 기본 경로
+//http://localhost:8080/swagger-ui/
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+
+//		// Gateway에서 SessionId -> session-profile로 오는 사용자 정보 헤더 매개변수
+//		RequestParameter sessionProfile = new RequestParameterBuilder()
+//				.name("session-profile")
+//				.description("{\"userId\":\"kdkcom\", \"role\":\"USER\"}")
+//				.required(false)
+//				.in("header")
+//				.build();
+//
+//		List<RequestParameter> params = new ArrayList<RequestParameter>();
+//		params.add(sessionProfile);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+//					.globalRequestParameters(params)	// 매개변수 추가
+                .apiInfo(apiInfo())
+                .select()
+                // 전체, actuator, error 등 포함 됨
+//					.apis(RequestHandlerSelectors.any())
+                // REST Controller만
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                // 전체 경로
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SubscribeService")
+                .version("1.0")
+                .description("API List of SubscribeService Application")
+                .license("포트폴리오 프로젝트")
+                .build();
+    }
+}
--- subscribe-server/subscribe-server/src/main/java/com/git/subscribeserver/configuration/WebConfiguration.java
+++ subscribe-server/subscribe-server/src/main/java/com/git/subscribeserver/configuration/WebConfiguration.java
@@ -17,8 +17,8 @@ public class WebConfiguration implements WebMvcConfigurer {
 				// origin(원천): html문서를 배포한 서버의 주소
 				// html문서에는 어디서 문서를 받아왔는지를 기록하고 있음
 				// 실제 통신은 브라우저 -> 서버
-				.allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500/",
-						"http://ec2-15-165-17-21.ap-northeast-2.compute.amazonaws.com")
+				//.allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500/",
+				//		"http://ec2-15-165-17-21.ap-northeast-2.compute.amazonaws.com")
 				// 공유정책으로 허용할 HTTP메서드
 				.allowedMethods("*"); // 전체메서드를 허용(GET, POST, PUT....)
 	}
